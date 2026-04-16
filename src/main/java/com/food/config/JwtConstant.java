3c9a9ac02c6fc86cf76b373e7892e9c8bf2b1c79
Kapil9399 <jadamkapil2004@gmail.com>
2026-04-16 17:52:13 +0530

    Initial commit: Online Food Ordering Spring Boot project JWT Authentication and Authorization

---
+++ src/main/java/com/food/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.food.config;
+
+public class JwtConstant {
+
+	public static final String SECRET_KEY="sfjdjfdfsdskjkdwroiwuuwnvnnkekorutiureyvbcvmiwj23583275nfmsdnskjfhwr0wruiowefh";
+	public static final String JWT_HEADER ="Authorization";
+}
