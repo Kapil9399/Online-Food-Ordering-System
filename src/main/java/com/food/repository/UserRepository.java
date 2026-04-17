96c67d9222fc8b0f8e74fd121a5751b929606648
Kapil9399 <jadamkapil2004@gmail.com>
2026-04-17 15:50:10 +0530

    add: User service , repository , request , response

---
+++ src/main/java/com/food/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.food.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.food.models.User;
+
+public interface UserRepository  extends JpaRepository<User,Long>{
+
+	public User findByEmail(String username);
+}
