46804184416c52b6cbbfaf7c076569056dc618e8
Kapil9399 <jadamkapil2004@gmail.com>
2026-05-12 12:42:29 +0530

    change in Restaurant model , change in images field

--- src/main/java/com/food/models/Restaurant.java
+++ src/main/java/com/food/models/Restaurant.java
@@ -6,16 +6,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -51,8 +42,14 @@ public class Restaurant {
 	@JsonIgnore
 	private List<Order> orders = new ArrayList<>();
 
-	@ElementCollection
+	/*@ElementCollection
 	@Column(length=1000)
+	private List<String> images;*/
+
+	// ✅ FIXED
+	@ElementCollection
+	@CollectionTable(name = "restaurant_images", joinColumns = @JoinColumn(name = "restaurant_id"))
+	@Column(name = "image_url", columnDefinition = "TEXT")
 	private List<String> images;
 	
 	private LocalDateTime registrationDate;
