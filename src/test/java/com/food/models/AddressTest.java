9136d13c11489be8ecb4d8121894d616223141d9
Kapil9399 <jadamkapil2004@gmail.com>
2026-05-12 12:25:21 +0530

    add getter setter test cases for address entity

--- src/test/java/com/food/models/AddressTest.java
+++ src/test/java/com/food/models/AddressTest.java
@@ -3,6 +3,7 @@ package com.food.models;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class AddressTest {
 
@@ -24,4 +25,26 @@ public class AddressTest {
         assertEquals("452010", address.getPincode());
         assertEquals("India", address.getCountry());
     }
+
+    @Test
+    void testGetterSetter()
+    {
+     Address address = new Address();
+
+     address.setId(1L);
+     address.setStreetAddress("Nanda Nagar");
+     address.setCity("indore");
+     address.setState("Madhya Pradesh");
+     address.setPincode("452010");
+     address.setCountry("India");
+
+        assertEquals(1L, address.getId());
+        assertEquals("Nanda Nagar", address.getStreetAddress());
+        assertEquals("indore", address.getCity());
+        assertEquals("Madhya Pradesh", address.getState());
+        assertEquals("452010", address.getPincode());
+        assertEquals("India", address.getCountry());
+    }
+
+
 }
